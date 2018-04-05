38f88e2a1ee305713d59ed7427108b4aed11cc6e
Jack Ivanov <e601809@gmail.com>
2018-04-05 15:38:05 +0300

    add version

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-provider "aws" {
-  version = "~> 1.9 "
-}
-
 terraform {
   required_version = "~> 0.11.3"
+}
+
+provider "aws" {
+  version = "~> 1.9"
+}
+
+provider "local" {
+  version = "~> 1.1"
+}
+
+provider "random" {
+  version = "~> 1.1"
+}
+
+provider "template" {
+  version = "~> 1.0"
+}
+
+provider "tls" {
+  version = "~> 1.0"
 }
